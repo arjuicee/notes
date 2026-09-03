93f9fcf6dc9dabc6ab3d0c0d7864b4a0a783ab71
Arju <arjunnp6@gmail.com>
2026-09-04 00:00:07 +0530

    updated footer attribution

--- app/loading_.tsx
+++ app/loading_.tsx
@@ -1,18 +1,31 @@
 "use client"
+
 import { Logo } from '@/components/logo';
 
 export default function Loading({msg}:{msg: string}) {
     return (
-        <div className={`flex gap-3 text-white h-[100dvh] w-screen flex-col z-50`}>
+        <div className="flex gap-3 text-white h-[100dvh] w-screen flex-col z-50">
             <div className="flex justify-center items-center h-full w-full transition-all drop-shadow-xl">
                 <Logo className={"size-80"}/>
-            </div> 
+            </div>
+
             <div className="flex justify-center flex-col items-center mb-10 gap-2 text-muted-foreground text-center">
                 <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-white mb-4"></div>
+
                 <small>Made with 💖 by μLearn UCEK</small>
+
+                <small>
+                    Forked by{" "}
+                    <a
+                        href="https://github.com/arjuicee/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-200 hover:text-gray-200 transition-all"
+                    >
+                        arjuicee
+                    </a>
+                </small>
             </div>
         </div>
     )
-  }
-
-  
+}
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,35 @@ export default function Footer() {
   return (
     <div className="fixed bottom-0 left-0 w-full px-4 sm:px-6 py-2 text-white flex flex-col justify-center items-center">
       <p className="text-xs sm:text-sm text-center">
-        © {new Date().getFullYear()} - All rights reserved ● {" "}
+        © {new Date().getFullYear()} - All rights reserved ●{" "}
         <Link
-          href={"/contributors"}
+          href="/contributors"
           className="text-xs sm:text-sm font-medium underline"
         >
           Contributors
         </Link>
-      </p>{" "}
+      </p>
+
       <p className="text-xs sm:text-sm font-semibold text-center">
-        Designed and developed by <Link href={"https://mulearn.uck.ac.in"} className="hover:text-gray-200 text-blue-200 hover:shadow transition-all">μLearn UCEK</Link>
+        Designed and developed by{" "}
+        <Link
+          href="https://mulearn.uck.ac.in"
+          className="hover:text-gray-200 text-blue-200 hover:shadow transition-all"
+        >
+          μLearn UCEK
+        </Link>
+      </p>
+
+      <p className="text-xs sm:text-sm text-center">
+        Forked by{" "}
+        <Link
+          href="https://github.com/arjuicee/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium text-blue-200 hover:text-gray-200 hover:shadow transition-all"
+        >
+          arjuicee
+        </Link>
       </p>
     </div>
   );
